e814c0f7e953a47d923fb73619829cfd11c49ec6
Jay0103l <jaylathiya1548@gmail.com>
2024-04-07 17:26:21 +0530

    add user registration flow

---
+++ resources/views/emails/WelcomeEmail.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Welcome Email</title>
+</head>
+
+<body>
+    <!-- <p>Hello, {{ $username }}</p> -->
+    <p>Welcome to Focus coolant and lubricants private limited</p>
+</body>
+
+</html>
